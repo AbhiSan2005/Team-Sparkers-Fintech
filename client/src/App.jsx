a0b6039cce527384ec85ee5d69d377a002351187
Krishna Warfade <krishnawarfade@gmail.com>
2025-08-09 10:21:13 +0530

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import React from 'react';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Routes>
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </div>
   )
